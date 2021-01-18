35d8ac337cfa7cdda74384079d9f14af35d012c6
LAPTOP-776AMTE0\carlo.figueroa <carlo.figueroa@sistemassa.cl>
2021-01-18 20:22:15 -0300

    Se actualiza DiezxcientoTest

--- src/test/java/ejemplopipeline/DiezxcientoTest.java
+++ src/test/java/ejemplopipeline/DiezxcientoTest.java
@@ -14,12 +14,13 @@ public class DiezxcientoTest {
 
 	@Test
 	public void testImpuesto() throws Exception {
-
+		Diezxciento diezxciento = new Diezxciento(23000000,900000);
+		assertEquals(0, diezxciento.impuesto());
 	}
 
 	@Test
 	public void testSaldoAhorro() throws Exception {
-
+		Diezxciento diezxciento = new Diezxciento(23000000,900000);
 	}
 
 }
