9000cd243d0d2ca729192cd44523b05e75b9c470
Carlos M. Riveros C <carlos.riveros@gmail.com>
2021-01-18 20:41:35 -0300

    Update testjava

--- src/test/java/ejemplopipeline/DiezxcientoTest.java
+++ src/test/java/ejemplopipeline/DiezxcientoTest.java
@@ -4,24 +4,26 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-public class DiezxcientoTest {
-
+public class DiezxcientoTest
+{
 	@Test
-	public void testDiezxciento() throws Exception {
+	public void testDiezxciento() throws Exception
+	{
 		Diezxciento diezxciento = new Diezxciento(23000000,900000);
 		assertEquals(2300000, diezxciento.diezxciento());
 	}
 
 	@Test
-	public void testImpuesto() throws Exception {
+	public void testImpuesto() throws Exception
+	{
 		Diezxciento diezxciento = new Diezxciento(23000000,900000);
 		assertEquals(0, diezxciento.impuesto());
 	}
 
 	@Test
-	public void testSaldoAhorro() throws Exception {
+	public void testSaldoAhorro() throws Exception
+	{
 		Diezxciento diezxciento = new Diezxciento(23000000,900000);
-		assertEquals(0, diezxciento.saldoAhorro());
+		assertEquals(20700000, diezxciento.saldoAhorro());
 	}
-
 }
